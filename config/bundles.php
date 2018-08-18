2acf589b75bdb30d63342c64e280a906361c6236
Christophe Villeger <villeger.c@gmail.com>
2018-08-18 21:30:48 +0200

    Users

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
