6304e0b0a3efcbba6dbe0032d574094a065e43cd
Christophe Villeger <villeger.c@gmail.com>
2018-08-21 20:24:31 +0200

    validation

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -8,7 +8,10 @@ use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 class UsersController extends FOSRestController
 {
@@ -36,8 +39,16 @@ class UsersController extends FOSRestController
      * @Rest\Post("/users")
      * @ParamConverter("user", converter="fos_rest.request_body")
      */
-    public function postUsersAction(User $user)
+    public function postUsersAction(User $user, ConstraintViolationListInterface $validationErrors)
     {
+        if ($validationErrors->count() > 0) {
+            /** @var ConstraintViolation $constraintViolation */
+            foreach ($validationErrors as $constraintViolation){
+                $message = $constraintViolation->getMessage(); // Returns the violation message. (Ex. This value should not be blank.)
+                $propertyPath = $constraintViolation->getPropertyPath(); // Returns the property path from the root element to the violation. (Ex. lastname)
+            }
+        }
+
         $this->em->persist($user);
         $this->em->flush();
         return $this->view($user);
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -58,16 +58,10 @@ class User implements UserInterface
      */
     private $apiKey;
 
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\Article", mappedBy="user")
-     */
-    private $articles;
-
     public function __construct()
     {
         $this->roles = array('ROLE_USER');
         $this->apiKey = uniqid('', true);
-        $this->articles = new ArrayCollection();
     }
 
     public function getId()
@@ -164,36 +158,5 @@ class User implements UserInterface
 
     public function eraseCredentials()
     {
-    }
-
-    /**
-     * @return Collection|Article[]
-     */
-    public function getArticles(): Collection
-    {
-        return $this->articles;
-    }
-
-    public function addArticle(Article $article): self
-    {
-        if (!$this->articles->contains($article)) {
-            $this->articles[] = $article;
-            $article->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeArticle(Article $article): self
-    {
-        if ($this->articles->contains($article)) {
-            $this->articles->removeElement($article);
-            // set the owning side to null (unless already changed)
-            if ($article->getUser() === $this) {
-                $article->setUser(null);
-            }
-        }
-
-        return $this;
     }
 }
